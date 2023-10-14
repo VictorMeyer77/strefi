9c0e5dbcf42d432c2747e2002f09372e17c6cfdc
victor <victormeyerc2i@gmail.com>
2023-10-14 21:33:06 +0200

    docs: add docstring and typing to parser

--- strefi/parser.py
+++ strefi/parser.py
@@ -1,7 +1,26 @@
+"""Provide methods to dynamically read line of a file.
+
+The module contains the following functions:
+
+- `yield_last_line(file, running_path)` - Yield last line of a file.
+- `stream_file(file_path, running_path)` - Wait file creation before calling yield_last_line.
+
+"""
+
 import os
+from typing import Iterator, TextIO
 
 
-def yield_last_line(file, running_path):
+def yield_last_line(file: TextIO, running_path: str) -> Iterator[str]:
+    """Yield last line of a file.
+
+    Args:
+        file: Read-open file to stream.
+        running_path: Path of running file. The function terminate when it's removed.
+
+    Returns:
+        Yield the last line. Ignore empty line.
+    """
     file.seek(0, os.SEEK_END)
     while os.path.exists(running_path):
         line = file.readline()
@@ -9,7 +28,17 @@ def yield_last_line(file, running_path):
             yield line
 
 
-def stream_file(file_path, running_path):
+def stream_file(file_path: str, running_path: str) -> Iterator[str]:
+    """Wait file creation before calling yield_last_line.
+        Choose this method if you want stream log file which doesn't exist yet.
+
+    Args:
+        file_path: File path to stream.
+        running_path: Path of running file. The function terminate when it's removed.
+
+    Returns:
+        Yield the last line. Ignore empty line.
+    """
     while os.path.exists(running_path):
         if os.path.exists(file_path):
             with open(file_path, "r") as file:
