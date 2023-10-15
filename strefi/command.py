995bc2b1c1ffd7ff50762e1c37b278979a4539d3
victor <victormeyerc2i@gmail.com>
2023-10-15 16:36:25 +0200

    docs: add docstring to command

--- strefi/command.py
+++ strefi/command.py
@@ -1,3 +1,16 @@
+"""Provides strefi main functions.
+
+The module contains the following functions:
+
+- `stream_file_to_topic(file_path, producer, topic, defaults, headers, running_path)` -
+Stream file and write last row to a kafka topic.
+- `create_threads(config)` - Create one thread by table from all the configuration.
+- `start(config_path)` - Read configuration file and launch all streams.
+- `stop(jobid)` - Stop strefi threads.
+"""
+
+from kafka import KafkaProducer
+
 import kafka_utils
 import threading
 import parser
@@ -5,16 +18,41 @@ import json
 import stopper
 
 
-def stream_file_to_topic(file_path, producer, topic, defaults, headers, running_path):
+def stream_file_to_topic(
+    file_path: str,
+    producer: KafkaProducer,
+    topic: str,
+    defaults: dict[str, object],
+    headers: dict[str, object],
+    running_path: str,
+):
+    """Stream file and write last row to a kafka topic.
+
+    Args:
+        file_path: File path to stream.
+        producer: Instance of KafkaProducer.
+        topic: Name of the target topic.
+        defaults: Configured dictionary to add in the record value.
+        headers: Configured headers dictionary.
+        running_path: Running file path
+    """
     for line in parser.stream_file(file_path, running_path):
         producer.send(
             topic,
             kafka_utils.format_record_value(file_path, line, defaults).encode(),
-            headers=headers,
+            headers=headers
         )
 
 
-def create_threads(config):
+def create_threads(config: dict[str, any]) -> list[threading.Thread]:
+    """Create one thread by table from all the configuration.
+
+    Args:
+        config: dictionary loads from configuration file.
+
+    Returns:
+        List composed by one thread per file to stream.
+    """
     threads = []
     headers = kafka_utils.format_record_headers(config["headers"])
     running_path = stopper.write_running_file()
@@ -31,7 +69,13 @@ def create_threads(config):
     return threads
 
 
-def start(config_path):
+def start(config_path: str):
+    """Read configuration file and launch all streams.
+    This function is the entrypoint of stefi start command.
+
+    Args:
+        config_path: Configuration file path.
+    """
     with open(config_path, "r") as f:
         config = json.loads(f.read())
     threads = create_threads(config)
@@ -39,5 +83,11 @@ def start(config_path):
     [thread.join() for thread in threads]
 
 
-def stop(jobid):
+def stop(jobid: str):
+    """Stop strefi threads.
+    This function is the entrypoint of strefi stop command.
+
+    Args:
+        jobid: ID of the stream to kill, 'all' to kill all streams.
+    """
     stopper.remove_running_file(jobid)
