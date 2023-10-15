361ecd5356dd5731959f0579457a4cfd2a6c1602
victor <victormeyerc2i@gmail.com>
2023-10-15 17:58:59 +0200

    docs: mkdocstring to __main__

--- strefi/__main__.py
+++ strefi/__main__.py
@@ -1,9 +1,27 @@
+"""Entrypoint of the program.
+    Provides functions to read arguments then call command.
+
+The module contains the following functions:
+
+- `parse_args(args)` - Read and verify program arguments.
+- `main(args)` - Entrypoint function. Launch the right method.
+"""
+
 import sys
 import argparse
 import command
 
 
-def parse_args(args):
+def parse_args(args: list[str]) -> argparse.Namespace:
+    """Read and verify program arguments.
+    Exit the program if arguments are invalid.
+
+    Args:
+        args: list of program arguments.
+
+    Returns:
+        Argparse namespace.
+    """
     parser = argparse.ArgumentParser(
         prog="strefi",
         description="Stream a file line per line and write in a kafka topic",
@@ -27,7 +45,12 @@ def parse_args(args):
     return args
 
 
-def main(args):
+def main(args: list[str]):
+    """Entrypoint function. Launch the right method.
+
+    Args:
+        args: list of program arguments.
+    """
     args = parse_args(args)
     if args.command.lower() == "start":
         command.start(args.config)
