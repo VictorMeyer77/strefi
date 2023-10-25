7c0781ce4746fa4d3866f006c0c15285d0205c45
victor <victormeyerc2i@gmail.com>
2023-10-25 15:38:48 +0200

    test: rename stopper to supervisor

--- tests/test_supervisor.py
+++ tests/test_supervisor.py
@@ -5,19 +5,19 @@ from strefi import supervisor
 
 
 def test_write_running_file_should_create_temp_file():
-    running_path = stopper.write_running_file()
+    running_path = supervisor.write_running_file("foo", "foo")
     assert os.path.exists(running_path)
 
 
 def test_remove_running_file_should_remove_one_temp_file():
-    running_path = stopper.write_running_file()
-    stopper.remove_running_file(running_path.split("_")[1])
+    running_path = supervisor.write_running_file("foo", "foo")
+    supervisor.remove_running_file(running_path.split("_")[1])
     assert not all(["strefi" in file for file in os.listdir(tempfile.gettempdir())])
 
 
 def test_remove_running_file_should_remove_all_temp_file():
-    stopper.write_running_file()
-    stopper.write_running_file()
-    stopper.write_running_file()
-    stopper.remove_running_file("all")
+    supervisor.write_running_file("foo", "foo")
+    supervisor.write_running_file("foo", "foo")
+    supervisor.write_running_file("foo", "foo")
+    supervisor.remove_running_file("all")
     assert not all(["strefi" in file for file in os.listdir(tempfile.gettempdir())])
